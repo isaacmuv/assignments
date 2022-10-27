ccd9206eb1c4c05cba190d8dfa12f0302b341141
Isaac Muvughe <76930253+isaacmuv@users.noreply.github.com>
2022-10-27 11:02:05 +0200

    Add files via upload

---
+++ portfoli/script.js
@@ -0,0 +1,16 @@
+footer{
+  height: 50px; 
+  background-color: brown; 
+}
+main{
+height: 585px;
+background-color: bisque;
+
+}
+#lean{
+    background-color: aqua;
+    border-radius: 5px;
+    height: 30px;
+    width: 200px;
+    align-self: flex-start;
+}
